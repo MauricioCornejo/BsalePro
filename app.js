b30461370fe2ce9e11ef8bb603d7c21c292231d9
Mauricio <mauricio.cornejo@orbisdata.cl>
2023-08-18 17:00:15 -0400

    input validations

--- app.js
+++ app.js
@@ -280,13 +280,14 @@ app.post('/add_product', async (req, res) => {
             variant,
             measurement
         } = req.body;
-
+        const price_formated = new Intl.NumberFormat().format((Number(price_product)).toFixed(2)).replace(',', '.')
+        const stock_formated = new Intl.NumberFormat().format((Number(stock)).toFixed(2)).replace(',', '.')
         console.log("AGREGAR PRODUCTO", req.body)
         const client = await pool.connect();
         await client.query(
             `INSERT INTO flores.product 
-            ( id, sku, name, price, stock, type, variant, measurement) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+            ( id, sku, name, price, stock, type, variant, measurement, price_formated, stock_formated) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
             [
                 uuidv4(),
                 sku_product,
@@ -296,6 +297,8 @@ app.post('/add_product', async (req, res) => {
                 type_product,
                 variant,
                 measurement,
+                price_formated,
+                stock_formated
             ]
         );
         client.release();
@@ -336,10 +339,12 @@ app.post('/edit_product/:id', async (req, res) => {
             variant,
             measurement
         } = req.body;
+        const price_formated = new Intl.NumberFormat().format((Number(price_product)).toFixed(2)).replace(',', '.')
+        const stock_formated = new Intl.NumberFormat().format((Number(stock)).toFixed(2)).replace(',', '.')
         console.log("*****************LLEGUE AL POST EDITAR ************************************")
         const client = await pool.connect();
         await client.query(
-            `UPDATE flores.product SET sku = $1, name = $2, price = $3, type = $4, stock = $5, variant = $6, measurement = $7 WHERE id = $8`,
+            `UPDATE flores.product SET sku = $1, name = $2, price = $3, type = $4, stock = $5, variant = $6, measurement = $7, price_formated = $8, stock_formated = $9 WHERE id = $10`,
             [sku_product,
                 name_product,
                 price_product,
@@ -347,6 +352,8 @@ app.post('/edit_product/:id', async (req, res) => {
                 stock,
                 variant,
                 measurement,
+                price_formated,
+                stock_formated,
                 id]
         );
         client.release();
@@ -371,6 +378,14 @@ app.delete('/delete_product/:id', async (req, res) => {
         res.status(500).json({ success: false, message: 'Error al eliminar el registro' });
     }
 });
+
+
+
+/****************************************************************** */
+/* Sales */
+/****************************************************************** */
+
+
 
 app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
